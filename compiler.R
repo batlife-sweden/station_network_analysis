48a2a4130908343e7eea90e2a502e5116a32cac9
Monotremeancer <mullman89@gmail.com>
2024-01-08 21:59:28 +0100

    Expanded the ToDo-list

--- compiler.R
+++ compiler.R
@@ -1,10 +1,11 @@
 require(janitor)
 require(dplyr)
 require(readxl)
+require(stringr)
 
 data <- read_xlsx("goholm2023.xlsx")
 
-cleanup_names <- function(data){
+cleanup_column_names <- function(data){
    data <- data %>%
     clean_names() %>% 
     rename(
@@ -15,12 +16,25 @@ cleanup_names <- function(data){
    data
 }
 
+cleanup_species_names(data){
+  #TODO: Use str_to_title from stringr to convert everything to title case,
+  # removing the need to deal with upper/lower case issues.
+}
+
+clear_empty_rows <- function(data){
+  #TODO: Remove empty art_x entries
+  
+}
+
 arrange_data <- function(data){
   data_new <- data[,-c("art_2", "art_3")]
   data_spec_2 <- data[,-c("art_1", "art_3")]
   data_spec_3 <- data[,-c("art_1", "art_2")]
+  
+  # TODO: add a function that removes empty rows and then append the result
+  # to data_new
 }
 
 
-data <- cleanup_names(data)
-unique(data$art_1)
+data <- cleanup_column_names(data)
+unique(data$art_3)
