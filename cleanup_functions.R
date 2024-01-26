a83264f4891a6023a60496fe8dae2c809c0183cb
Monotremeancer <mullman89@gmail.com>
2024-01-26 16:55:32 +0100

    Changed so that NA is now represented as a zero-value for species count

--- cleanup_functions.R
+++ cleanup_functions.R
@@ -150,13 +150,14 @@ spread_by_date <- function(data,
                 values_from = n, values_fill = 0)
   
   # Join species info on time series
-  data <- left_join(time_series, species_count)  
+  # Replace NA with 0-values for species to make graphs smoother
+  data <- left_join(time_series, species_count) %>% 
+    replace(is.na(.), 0) 
+  
+  # Social calls are left with NA values so we do not have to plot hundreds
+  # of 0-values in a later stage
   data <- left_join(data, social_count)
   
-  
-  #TODO: find rows where social calls are NA, but  not obs.
-  # use test[logical vector,c("two","three")] <- list(rep(0, ncol(social)-1))[[1]]
-  # to set all the colums there (except date) as 0
   data
 }
 
@@ -173,5 +174,6 @@ sum_observations <- function(data){
 
 # Attach number of calls per night
 attach_obs_count <- function(data, n_recordings){
-  left_join(data,n_recordings)
+  data <-left_join(data,n_recording_day)
+  data$tot_obs_day[is.na(data$tot_obs_day)] <- 0 
 }
